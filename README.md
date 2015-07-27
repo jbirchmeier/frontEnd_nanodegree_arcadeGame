ba31727229d194b90b72899302c27037a14e50f9
jenbirch10 <jenbirch10@gmail.com>
2015-07-27 18:28:01 -0500

    revised to correct errors
    Includes instructions.

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ frontend-nanodegree-arcade-game
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
+
+Welcome to my game!
+
+The goal of the game is to move your character across the screen successfully without running in to any of those pesky bugs. 
+
+Use the arrow keys on your keyboard to move in any direction on the screen. Once you reach the water, you win! If you run in to a bug, the game will restart. 
+
+Have fun!
--- js/app.js
+++ js/app.js
@@ -1,46 +1,10 @@
-// Enemies our player must avoid
-var Enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.x = x; 
-    this.y = y;
-    this.sprite = 'images/enemy-bug.png';
-}
-
-
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.speed = Math.floor(Math.random() * 350 + 1);
-    this.x += this.speed * dt; 
-    if(this.x > 505) {
-        this.x = Math.random() - 550;
-    }
-}
-
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function(x,y) {
     this.sprite = 'images/char-horn-girl.png';
     
     this.x = 505/50 - 10;
     this.y = 380;
     
-}
+};
 
 
 Player.prototype.update = function(dt) {
@@ -50,16 +14,16 @@ Player.prototype.update = function(dt) {
 
     if(this.y < 0 ) {
         this.resetGame();
-        alert("You win!")
+        alert("You win!");
     }
 
-}
+};
 
 Player.prototype.collisionCheck = function() {
-    for (var i = 0; i < allEnemies.length; i++) {
-        if(Math.abs(player.x - allEnemies[i].x) < 30 && Math.abs(player.y - allEnemies[i].y) < 30) {
+    for (var i = 0; i < enemyLength; i++) {
+        if(Math.abs(this.x - allEnemies[i].x) < 30 && Math.abs(this.y - allEnemies[i].y) < 30) {
             this.resetGame();
-            alert("Those bugs will get you every time. Try again!")
+            alert("Those bugs will get you every time. Try again!");
         }
     }
 };
@@ -67,12 +31,12 @@ Player.prototype.collisionCheck = function() {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     console.log(this.sprite);
-}
+};
 
 Player.prototype.resetGame = function() {
     this.x = 505/50 - 10;
     this.y = 380;
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -82,10 +46,11 @@ var enemy1 = new Enemy(-30, 63);
 var enemy2 = new Enemy(-350, 145);
 var enemy3 = new Enemy(-96, 228);
 var enemy4 = new Enemy(-596, 63);
-var enemy5 = new Enemy(-550, 150)
-var enemy6 = new Enemy(-750, 220)
+var enemy5 = new Enemy(-550, 150);
+var enemy6 = new Enemy(-750, 220);
 
 var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+var enemyLength = allEnemies.length;
 
 var player = new Player();
 
