55e6b8413f1c709326e82feb19901a0e2daff8d6
jenbirch10 <jenbirch10@gmail.com>
2015-07-28 07:36:03 -0500

    add back cutoff enemy code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
-var Player = function(x,y) {
+
+// Enemies our player must avoid
+var Enemy = function(x,y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.x = x; 
+    this.y = y;
+    this.sprite = 'images/enemy-bug.png';
+}
+
+
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.speed = Math.floor(Math.random() * 350 + 1);
+    this.x += this.speed * dt; 
+    if(this.x > 505) {
+        this.x = Math.random() - 550;
+    }
+}
+
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.var Player = function(x,y) {
     this.sprite = 'images/char-horn-girl.png';
     
     this.x = 505/50 - 10;
